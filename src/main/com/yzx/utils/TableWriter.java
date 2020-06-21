51556614719bf60fb88671bf902423d43fd6670e
yezhangxiang <yezhangxiang@gmail.com>
2020-06-21 18:26:31 +0800

    feat: add customized spliterator

---
+++ src/main/com/yzx/utils/TableWriter.java
@@ -0,0 +1,32 @@
+package com.yzx.utils;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class TableWriter<T> {
+    private final Map<String, Function<T, String>> functionMap;
+    private final List<T> tList;
+    private final CharSequence delimiter;
+
+    public TableWriter(Map<String, Function<T, String>> functionMap, List<T> tList) {
+        this.functionMap = functionMap;
+        this.tList = tList;
+        this.delimiter = ",";
+    }
+
+    public String write() {
+        List<String> keys = new ArrayList<>(functionMap.keySet());
+        StringBuilder sb = new StringBuilder(String.join(delimiter, keys));
+        sb.append(System.lineSeparator());
+        for (T t : tList) {
+            List<String> items = new ArrayList<>();
+            for (String key : keys) {
+                Function<T, String> stringFunction = functionMap.get(key);
+                String s = stringFunction.apply(t);
+                items.add(s);
+            }
+            sb.append(String.join(delimiter, items)).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/com/yzx/utils/TableWriterTest.java
@@ -0,0 +1,43 @@
+package com.yzx.utils;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TableWriterTest {
+    @Test
+    public void write() throws Exception {
+        List<Cell> cells = IntStream.range(0, 10).mapToObj(i -> new Cell(i, "cell_" + String.valueOf(i))).collect(Collectors.toList());
+        Map<String, Function<Cell, String>> functionMap = new HashMap<>();
+        functionMap.put("name", Cell::getName);
+        functionMap.put("id", cell -> String.valueOf(cell.getId()));
+
+        TableWriter<Cell> tableWriter = new TableWriter<>(functionMap, cells);
+        System.out.println(tableWriter.write());
+    }
+
+}
+
+class Cell {
+    private final int id;
+    private final String name;
+
+    Cell(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
