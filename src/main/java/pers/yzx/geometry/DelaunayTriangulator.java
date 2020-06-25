36d0120bbe361444585df0050b1f1c1fb75a21dc
yezhangxiang <yezhangxiang@gmail.com>
2020-06-25 14:13:31 +0800

    feat: split big function

--- src/main/java/pers/yzx/geometry/DelaunayTriangulator.java
+++ src/main/java/pers/yzx/geometry/DelaunayTriangulator.java
@@ -1,15 +1,13 @@
 package pers.yzx.geometry;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Implementation of incremental Delaunay triangulation algorithm.only for 2D.
  */
 public class DelaunayTriangulator {
 
-    private List<Point> pointSet;
+    private Set<Point> pointSet;
     private TriangleSoup triangleSoup;
 
     /**
@@ -18,7 +16,7 @@ public class DelaunayTriangulator {
      *
      * @param pointSet The point set to be triangulated
      */
-    public DelaunayTriangulator(List<Point> pointSet) {
+    public DelaunayTriangulator(Set<Point> pointSet) {
         this.pointSet = pointSet;
         this.triangleSoup = new TriangleSoup();
     }
@@ -31,17 +29,97 @@ public class DelaunayTriangulator {
      */
     public void triangulate() throws IllegalArgumentException {
         triangleSoup = new TriangleSoup();
-
         if (pointSet == null || pointSet.size() < 3) {
             throw new IllegalArgumentException("Less than three points in point set.");
         }
-
+        Triangle superTriangle = generateSuperTriangle();
+        triangleSoup.add(superTriangle);
+        for (Point point : pointSet) {
+            insertOnePoint(point);
+        }
         /**
-         * In order for the in circumcircle test to not consider the vertices of
-         * the super triangle we have to start out with a big triangle
-         * containing the whole point set. We have to scale the super triangle
-         * to be very large. Otherwise the triangulation is not convex.
+         * Remove all triangles that contain vertices of the super triangle.
          */
+        triangleSoup.removeTrianglesUsing(superTriangle.a);
+        triangleSoup.removeTrianglesUsing(superTriangle.b);
+        triangleSoup.removeTrianglesUsing(superTriangle.c);
+    }
+
+    private void insertOnePoint(Point point) {
+        Triangle triangle = triangleSoup.findContainingTriangle(point);
+        if (triangle == null) {
+            pointOnEdge(point);
+        } else {
+            pointInsideTriangle(point, triangle);
+        }
+    }
+
+    /**
+     * The vertex is inside a triangle.
+     */
+    private void pointInsideTriangle(Point point, Triangle triangle) {
+        Point a = triangle.a;
+        Point b = triangle.b;
+        Point c = triangle.c;
+
+        triangleSoup.remove(triangle);
+
+        Triangle first = new Triangle(a, b, point);
+        Triangle second = new Triangle(b, c, point);
+        Triangle third = new Triangle(c, a, point);
+
+        triangleSoup.add(first);
+        triangleSoup.add(second);
+        triangleSoup.add(third);
+
+        legalizeEdge(first, new Edge(a, b), point);
+        legalizeEdge(second, new Edge(b, c), point);
+        legalizeEdge(third, new Edge(c, a), point);
+    }
+
+    /**
+     * If no containing triangle exists, then the vertex is not
+     * inside a triangle (this can also happen due to numerical
+     * errors) and lies on an edge. In order to find this edge we
+     * search all edges of the triangle soup and select the one
+     * which is nearest to the point we try to add. This edge is
+     * removed and four new edges are added.
+     */
+    private void pointOnEdge(Point point) {
+        Edge edge = triangleSoup.findNearestEdge(point);
+
+        Triangle first = triangleSoup.findOneTriangleSharing(edge);
+        Triangle second = triangleSoup.findNeighbour(first, edge);
+
+        Point firstNoneEdgeVertex = first.getNoneEdgeVertex(edge);
+        Point secondNoneEdgeVertex = second.getNoneEdgeVertex(edge);
+
+        triangleSoup.remove(first);
+        triangleSoup.remove(second);
+
+        Triangle triangle1 = new Triangle(edge.a, firstNoneEdgeVertex, point);
+        Triangle triangle2 = new Triangle(edge.b, firstNoneEdgeVertex, point);
+        Triangle triangle3 = new Triangle(edge.a, secondNoneEdgeVertex, point);
+        Triangle triangle4 = new Triangle(edge.b, secondNoneEdgeVertex, point);
+
+        triangleSoup.add(triangle1);
+        triangleSoup.add(triangle2);
+        triangleSoup.add(triangle3);
+        triangleSoup.add(triangle4);
+
+        legalizeEdge(triangle1, new Edge(edge.a, firstNoneEdgeVertex), point);
+        legalizeEdge(triangle2, new Edge(edge.b, firstNoneEdgeVertex), point);
+        legalizeEdge(triangle3, new Edge(edge.a, secondNoneEdgeVertex), point);
+        legalizeEdge(triangle4, new Edge(edge.b, secondNoneEdgeVertex), point);
+    }
+
+    /**
+     * In order for the in circumcircle test to not consider the vertices of
+     * the super triangle we have to start out with a big triangle
+     * containing the whole point set. We have to scale the super triangle
+     * to be very large. Otherwise the triangulation is not convex.
+     */
+    private Triangle generateSuperTriangle() {
         double maxOfAnyCoordinate = 0.0d;
 
         for (Point point : getPointSet()) {
@@ -53,89 +131,7 @@ public class DelaunayTriangulator {
         Point p1 = new Point(0.0d, 3.0d * maxOfAnyCoordinate);
         Point p2 = new Point(3.0d * maxOfAnyCoordinate, 0.0d);
         Point p3 = new Point(-3.0d * maxOfAnyCoordinate, -3.0d * maxOfAnyCoordinate);
-
-        Triangle superTriangle = new Triangle(p1, p2, p3);
-
-        triangleSoup.add(superTriangle);
-
-        /**
-         * If no containing triangle exists, then the vertex is not
-         * inside a triangle (this can also happen due to numerical
-         * errors) and lies on an edge. In order to find this edge we
-         * search all edges of the triangle soup and select the one
-         * which is nearest to the point we try to add. This edge is
-         * removed and four new edges are added.
-         */
-        /**
-         * The vertex is inside a triangle.
-         */
-        for (Point point : pointSet) {
-            Triangle triangle = triangleSoup.findContainingTriangle(point);
-
-            if (triangle == null) {
-                /**
-                 * If no containing triangle exists, then the vertex is not
-                 * inside a triangle (this can also happen due to numerical
-                 * errors) and lies on an edge. In order to find this edge we
-                 * search all edges of the triangle soup and select the one
-                 * which is nearest to the point we try to add. This edge is
-                 * removed and four new edges are added.
-                 */
-                Edge edge = triangleSoup.findNearestEdge(point);
-
-                Triangle first = triangleSoup.findOneTriangleSharing(edge);
-                Triangle second = triangleSoup.findNeighbour(first, edge);
-
-                Point firstNoneEdgeVertex = first.getNoneEdgeVertex(edge);
-                Point secondNoneEdgeVertex = second.getNoneEdgeVertex(edge);
-
-                triangleSoup.remove(first);
-                triangleSoup.remove(second);
-
-                Triangle triangle1 = new Triangle(edge.a, firstNoneEdgeVertex, point);
-                Triangle triangle2 = new Triangle(edge.b, firstNoneEdgeVertex, point);
-                Triangle triangle3 = new Triangle(edge.a, secondNoneEdgeVertex, point);
-                Triangle triangle4 = new Triangle(edge.b, secondNoneEdgeVertex, point);
-
-                triangleSoup.add(triangle1);
-                triangleSoup.add(triangle2);
-                triangleSoup.add(triangle3);
-                triangleSoup.add(triangle4);
-
-                legalizeEdge(triangle1, new Edge(edge.a, firstNoneEdgeVertex), point);
-                legalizeEdge(triangle2, new Edge(edge.b, firstNoneEdgeVertex), point);
-                legalizeEdge(triangle3, new Edge(edge.a, secondNoneEdgeVertex), point);
-                legalizeEdge(triangle4, new Edge(edge.b, secondNoneEdgeVertex), point);
-            } else {
-                /**
-                 * The vertex is inside a triangle.
-                 */
-                Point a = triangle.a;
-                Point b = triangle.b;
-                Point c = triangle.c;
-
-                triangleSoup.remove(triangle);
-
-                Triangle first = new Triangle(a, b, point);
-                Triangle second = new Triangle(b, c, point);
-                Triangle third = new Triangle(c, a, point);
-
-                triangleSoup.add(first);
-                triangleSoup.add(second);
-                triangleSoup.add(third);
-
-                legalizeEdge(first, new Edge(a, b), point);
-                legalizeEdge(second, new Edge(b, c), point);
-                legalizeEdge(third, new Edge(c, a), point);
-            }
-        }
-
-        /**
-         * Remove all triangles that contain vertices of the super triangle.
-         */
-        triangleSoup.removeTrianglesUsing(superTriangle.a);
-        triangleSoup.removeTrianglesUsing(superTriangle.b);
-        triangleSoup.removeTrianglesUsing(superTriangle.c);
+        return new Triangle(p1, p2, p3);
     }
 
     /**
@@ -171,33 +167,11 @@ public class DelaunayTriangulator {
     }
 
     /**
-     * Creates a random permutation of the specified point set. Based on the
-     * implementation of the Delaunay algorithm this can speed up the
-     * computation.
-     */
-    public void shuffle() {
-        Collections.shuffle(pointSet);
-    }
-
-    /**
-     * Shuffles the point set using a custom permutation sequence.
-     *
-     * @param permutation The permutation used to shuffle the point set
-     */
-    public void shuffle(int[] permutation) {
-        List<Point> temp = new ArrayList<>();
-        for (int value : permutation) {
-            temp.add(pointSet.get(value));
-        }
-        pointSet = temp;
-    }
-
-    /**
      * Returns the point set in form of a vector of 2D vectors.
      *
      * @return Returns the points set.
      */
-    public List<Point> getPointSet() {
+    public Set<Point> getPointSet() {
         return pointSet;
     }
 
@@ -210,5 +184,4 @@ public class DelaunayTriangulator {
     public List<Triangle> getTriangles() {
         return triangleSoup.getTriangles();
     }
-
 }
--- src/test/java/pers/yzx/geometry/DelaunayTriangulatorTest.java
+++ src/test/java/pers/yzx/geometry/DelaunayTriangulatorTest.java
@@ -2,8 +2,9 @@ package pers.yzx.geometry;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,7 +12,7 @@ public class DelaunayTriangulatorTest {
     @Test
     public void triangulate() throws Exception {
         double step = 200;
-        List<Point> points = new ArrayList<>();
+        Set<Point> points = new HashSet<>();
         for (double x = 0; x < 10000; x += step) {
             for (int y = 0; y < 10000; y += step) {
                 points.add(new Point(x, y));
